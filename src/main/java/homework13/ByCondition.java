fb294d33f4f88f98b655ab5b3598438673363e26
Viktor Loskutov <vikandloskutov@gmail.com>
2021-11-07 10:46:34 +0300

    add hw-13

---
+++ src/main/java/homework13/ByCondition.java
@@ -0,0 +1,5 @@
+package homework13;
+
+interface ByCondition {
+    boolean isOk(int number);
+}
---
+++ src/main/java/homework13/Main.java
@@ -0,0 +1,22 @@
+package homework13;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static final int[] ARRAY = new int[] {1, 2, 11, 22, 333, 444, 0, -10, - 20, 1000, 271, 14, 33, 197};
+    public static void main(String[] args) {
+
+
+        int[] flt1 = Sequence.filter(ARRAY, value -> value % 2 == 0 && value != 0);
+        System.out.println(Arrays.toString(flt1));
+
+        int[] flt2 = Sequence.filter(ARRAY, value -> sumDigits(value) % 2 == 0);
+        System.out.println(Arrays.toString(flt2));
+
+    }
+
+    private static int sumDigits(int i) {
+        return i == 0 ? 0 : i % 10 + sumDigits(i / 10);
+    }
+}
