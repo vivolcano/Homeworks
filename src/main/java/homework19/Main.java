f153997fcdb61fda94f00433a9c61cf26583275a
Viktor Loskutov <vikandloskutov@gmail.com>
2021-11-18 01:08:54 +0300

    add hw-20

--- src/main/java/homework19/Main.java
+++ src/main/java/homework19/Main.java
@@ -3,7 +3,7 @@ package homework19;
 public class Main {
     public static void main(String[] args) {
         UsersRepository usersRepository =
-                new UsersRepositoryFileImpl("/Users/admin/Desktop/Homeworks/src/main/resources/users.txt");
+                new UsersRepositoryFileImpl("/Users/admin/Desktop/Homeworks/src/main/resources/homework19_users.txt");
 
         usersRepository.findByAge(64).stream().forEach(System.out::println);
         usersRepository.findByIsWorkerIsTrue().stream().forEach(System.out::println);
