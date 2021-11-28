6c76c82a0018503667c11834fef7830e65fd16f8
Viktor Loskutov <vikandloskutov@gmail.com>
2021-11-28 23:48:11 +0300

    add readme file

--- src/test/java/homeworks/homework26/NumbersUtilTest.java
+++ src/test/java/homeworks/homework26/NumbersUtilTest.java
@@ -11,31 +11,52 @@ import java.util.stream.Stream;
 class NumbersUtilTest {
 
     @ParameterizedTest
-    @MethodSource("factory")
-    @DisplayName("Проверка метода поиса наибольшего общего делителя")
-    void gcdWithParams(int firstValue, int secondValue, int expected) {
+    @MethodSource("factory1")
+    @DisplayName("Проверка метода поиска наибольшего общего делителя")
+    void gcdWithParamsEquals(int firstValue, int secondValue, int expected) {
 
         final int actual = NumbersUtil.gcd(firstValue, secondValue);
         Assertions.assertEquals(actual, expected);
     }
 
-    private static Stream <Arguments> factory() {
+    private static Stream <Arguments> factory1() {
         return Stream.of(
                 Arguments.of(18, 12, 6),
                 Arguments.of(9, 12, 3),
                 Arguments.of(64, 48, 16));
     }
 
-    @Test
-    @DisplayName("Проверка на выброс исключения при вводе отрицательного числа")
-    void gcdException() {
+    @ParameterizedTest
+    @MethodSource("factory2")
+    @DisplayName("Проверка метода поиса наибольшего общего делителя с неверным результатом")
+    void gcdWithParamsNotEquals(int firstValue, int secondValue, int expected) {
 
-        final int firstNumber = -27;
-        final int secondNumber = 24;
+        final int actual = NumbersUtil.gcd(firstValue, secondValue);
+        Assertions.assertNotEquals(actual, expected);
+    }
+
+    private static Stream <Arguments> factory2() {
+        return Stream.of(
+                Arguments.of(18, 12, 5),
+                Arguments.of(9, 12, 2),
+                Arguments.of(64, 48, 15));
+    }
+
+    @ParameterizedTest
+    @MethodSource("factory3")
+    @DisplayName("Проверка на выброс исключения при вводе отрицательного числа")
+    void gcdException(int firstNumber, int secondNumber) {
 
         Assertions.assertThrows(
                 ArithmeticException.class,
                 () -> NumbersUtil.gcd(firstNumber, secondNumber)
         );
+    }
+
+    private static Stream <Arguments> factory3() {
+        return Stream.of(
+                Arguments.of(18, -12),
+                Arguments.of(-9, 12),
+                Arguments.of(-64, -48));
     }
 }
